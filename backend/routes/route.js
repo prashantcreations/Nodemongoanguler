de0ab1b62d73ee2b3ab83f0cf569f0d948e97d42
Prashant Tiwari <prashant.tiwari@yash.com>
2020-01-17 15:04:39 +0530

    add all

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var category = require('../controller/categoryController.js');
+var expense = require('../controller/expenseController.js');
 
 router.get('/categories', category.getCategories);
 router.post('/addCategories', category.addCategories);
 router.put('/updateCategories/:id', category.updateCategories);
 router.delete('/deleteCategories/:id', category.deleteCategories);
+
+
+router.get('/expense', expense.getexpense);
+router.post('/addExpense', expense.addExpense);
+router.put('/updateExpense/:id', expense.updateExpense);
+router.delete('/deleteExpense/:id', expense.deleteExpense);
+
 
 module.exports = router
